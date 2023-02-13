efa84b09ad9f033a4ec9a93d4a7554bfebf60a72
Joneilson Junior <joneilsonjunior@gmail.com>
2023-02-12 21:27:42 -0300

    Tabuleiro e Pecas criadas

---
+++ src/boardgame/Board.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Board {
+	
+	private int linhas;
+	private int colunas;
+	private Piece [][] pecas;
+	
+	public Board(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		
+		pecas = new Piece [linhas] [colunas];
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+	
+	
+	
+
+}
